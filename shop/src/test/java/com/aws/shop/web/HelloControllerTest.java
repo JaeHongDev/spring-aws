8c83411cfbb8952239f51f3ee8cab2df3b5daf60
JaeHongDev <println@kakao.com>
2021-09-05 07:55:26 +0000

    spring test init

---
+++ shop/src/test/java/com/aws/shop/web/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.aws.shop.web;
+
+import com.aws.shop.HelloController;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+  @Autowired
+  private MockMvc mvc;
+
+  @Test
+  public void return_hello() throws Exception {
+    String hello = "Hello";
+    mvc.perform(
+      MockMvcRequestBuilders.get("/hello")
+    ).andExpect(MockMvcResultMatchers.status().isOk())
+    .andExpect(MockMvcResultMatchers.content().string(hello));
+  }
+}
